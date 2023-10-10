a68b1533d999c3ff0ea6df550e408710fc2d07e5
Jafet Castillo <jafetkaid@gmail.com>
2023-10-10 15:23:36 -0600

    Add SigIn screen

--- App.js
+++ App.js
@@ -1,96 +1,9 @@
-import React, { useState } from 'react';
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
 
-import { Colors } from './src/assets/colors/mainColors';
-
-import { FirstTextInput } from './src/components/TextInput/FirstTextInput';
-import GenericButton from './src/components/Buttons/GenericButton';
+import SignIn from './src/screens/SignIn';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [isTextInputSelected, setIsTextInputSelected] = useState(false);
-
-  const onSelecting = isSelected => {
-    setIsTextInputSelected(isSelected);
-  };
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.tyrianPurple : Colors.peach,
-    flex: 1,
-  };
-  const styles = StyleSheet.create({
-    firstSectionContainer: {
-      flex: 1,
-    },
-    container: {
-      flex: 1,
-      paddingHorizontal: 25,
-    },
-    titleTextContainer: {
-      flex: 1,
-      alignItems: 'flex-start',
-      justifyContent: 'flex-end',
-      marginBottom: 20,
-    },
-    titleText: {
-      fontSize: 46,
-      color: isDarkMode ? Colors.peach : Colors.tyrianPurple,
-    },
-    secondSectionContainer: {
-      flex: isTextInputSelected ? 2 : 1,
-    },
-    textInputSection: { flex: 1, width: '100%', paddingVertical: 20 },
-    thirdSection: {
-      flex: 1,
-      paddingTop: 0,
-    },
-  });
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <KeyboardAvoidingView
-        style={backgroundStyle}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={styles.container}>
-            <View style={styles.firstSectionContainer}>
-              <View style={styles.titleTextContainer}>
-                <Text style={styles.titleText}>Hello there</Text>
-              </View>
-            </View>
-            <View style={styles.secondSectionContainer}>
-              <View style={styles.textInputSection}>
-                <FirstTextInput
-                  valueName={'Second Value'}
-                  placeHolderName={'Second Input'}
-                  onSelecting={onSelecting}
-                />
-                <FirstTextInput
-                  valueName={'Second Value'}
-                  placeHolderName={'Second Input'}
-                  onSelecting={onSelecting}
-                />
-              </View>
-            </View>
-            <View style={styles.thirdSection}>
-              <GenericButton />
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
-  );
+  return <SignIn />;
 };
 
 export default App;
