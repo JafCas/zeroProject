0b28ecb7ab0a7ede1f2f02d5c5879ff47acde1e0
Jafet Castillo <jafetkaid@gmail.com>
2023-10-10 15:00:18 -0600

    Add animated shadow for text input box

--- src/components/TextInput/FirstTextInput.js
+++ src/components/TextInput/FirstTextInput.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import {
+  Animated,
   StyleSheet,
   Text,
   TextInput,
@@ -11,24 +12,46 @@ import { Colors } from '../../assets/colors/mainColors';
 export const FirstTextInput = ({ valueName, placeHolderName, onSelecting }) => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const [textInputBorderWith, setTextInputBorderWith] = useState(0);
+  const fadeBorder = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 0.5 seconds
+    Animated.timing(fadeBorder, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 0.5 seconds
+    Animated.timing(fadeBorder, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.textInputNameContainer}>
         <Text style={styles.textInputName(isDarkMode)}>{placeHolderName}</Text>
       </View>
-      <TextInput
-        placeholder={valueName}
-        style={styles.textInputBox(textInputBorderWith, isDarkMode)}
-        onFocus={() => {
-          setTextInputBorderWith(2);
-          onSelecting(true);
-        }}
-        onEndEditing={() => {
-          setTextInputBorderWith(0);
-          onSelecting(false);
-        }}
-      />
+      <View>
+        <TextInput
+          placeholder={valueName}
+          style={styles.textInputBox(isDarkMode)}
+          onFocus={() => {
+            onSelecting(true);
+            fadeIn();
+          }}
+          onEndEditing={() => {
+            onSelecting(false);
+            fadeOut();
+          }}
+        />
+        <Animated.View style={styles.animatedShadow(isDarkMode, fadeBorder)} />
+      </View>
     </View>
   );
 };
@@ -37,23 +60,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    marginVertical: 1,
   },
   textInputNameContainer: {
     marginLeft: 10,
-    marginBottom: 4,
   },
   textInputName: isDarkMode => ({
     color: isDarkMode ? Colors.peach : Colors.tyrianPurple,
     fontSize: 13,
     fontWeight: '600',
   }),
-  textInputBox: (textInputBorderWith, isDarkMode) => ({
+  textInputBox: isDarkMode => ({
     backgroundColor: isDarkMode ? '#58002C' : '#F8E9C9',
     padding: 10,
-    borderBottomWidth: textInputBorderWith,
-    borderRightWidth: textInputBorderWith,
     borderRadius: 5,
-    borderColor: '#C28F21',
     opacity: 50,
+  }),
+  animatedShadow: (isDarkMode, fadeBorder) => ({
+    opacity: fadeBorder,
+    backgroundColor: isDarkMode ? '#96014c' : '#C28F21',
+    position: 'absolute',
+    top: 2,
+    left: 2,
+    width: '100%',
+    height: '100%',
+    zIndex: -1,
+    borderRadius: 6,
   }),
 });
