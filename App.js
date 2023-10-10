39533a3b6c61d744980f2493a739ef0060f6e208
Jafet Castillo <jafetkaid@gmail.com>
2023-10-10 11:00:32 -0600

    Add keyboardAvoidingView component

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableWithoutFeedback,
   useColorScheme,
   View,
 } from 'react-native';
@@ -12,14 +18,15 @@ import {FirstTextInput} from './src/components/TextInput/FirstTextInput';
 import {Colors} from './src/interface/colors/mainColors';
 
 const App = () => {
-  const [buttonColor, setButtonColor] = useState(Colors.tyrianPurple);
   const isDarkMode = useColorScheme() === 'dark';
+  const [buttonColor, setButtonColor] = useState(
+    isDarkMode ? Colors.peach : Colors.tyrianPurple,
+  );
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.jordyBlue : Colors.peach,
+    backgroundColor: isDarkMode ? Colors.tyrianPurple : Colors.peach,
     flex: 1,
   };
-
   const styles = StyleSheet.create({
     titleContainer: {
       flex: 1,
@@ -32,63 +39,103 @@ const App = () => {
       justifyContent: 'flex-end',
       marginBottom: 20,
     },
-    titleText: {fontSize: 46},
+    titleText: {
+      fontSize: 46,
+      color: isDarkMode ? Colors.peach : Colors.tyrianPurple,
+    },
     secondSectionContainer: {
       flex: 1,
     },
     textInputSection: {flex: 1, width: '100%', paddingVertical: 20},
     thirdSection: {flex: 1, paddingTop: 40},
+    //
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: 'white',
+    },
+    textInputNameContainer: {
+      marginLeft: 10,
+      marginBottom: 4,
+    },
+    textInputName: {
+      color: isDarkMode ? Colors.peach : Colors.tyrianPurple,
+      fontSize: 13,
+      fontWeight: '600',
+    },
+    textInputBox: {
+      backgroundColor: isDarkMode ? '#58002C' : '#F8E9C9',
+      padding: 10,
+      borderBottomWidth: 2,
+      borderRightWidth: 2,
+      borderRadius: 5,
+      borderColor: '#C28F21',
+      opacity: 50,
+    },
+    //
   });
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <View style={styles.titleContainer}>
-        <View style={styles.titleTextContainer}>
-          <Text style={styles.titleText}>Hello there</Text>
-        </View>
-        <View style={styles.secondSectionContainer}>
-          <View style={styles.textInputSection}>
-            <FirstTextInput
-              valueName={'First Value'}
-              placeHolderName={'First Input'}
-            />
-            <FirstTextInput
-              valueName={'Second Value'}
-              placeHolderName={'Second Input'}
-            />
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 10 : 0}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.titleContainer}>
+            <View style={styles.titleTextContainer}>
+              <Text style={styles.titleText}>Hello there</Text>
+            </View>
+            <View style={styles.secondSectionContainer}>
+              <View style={styles.textInputSection}>
+                <FirstTextInput
+                  valueName={'Second Value'}
+                  placeHolderName={'Second Input'}
+                />
+                <FirstTextInput
+                  valueName={'Second Value'}
+                  placeHolderName={'Second Input'}
+                />
+              </View>
+            </View>
+            <View style={styles.thirdSection}>
+              <Pressable
+                style={{
+                  backgroundColor: buttonColor,
+                  marginHorizontal: '30%',
+                  borderRadius: 30,
+                  padding: 10,
+                  alignItems: 'center',
+                  elevation: 3,
+                }}
+                onPressIn={() => {
+                  setButtonColor(Colors.rose);
+                  console.log('in');
+                }}
+                onPressOut={() => {
+                  setButtonColor(
+                    isDarkMode ? Colors.peach : Colors.tyrianPurple,
+                  );
+                  console.log('out');
+                }}
+                onPress={() => {
+                  console.log('first');
+                }}>
+                <Text
+                  style={{
+                    color: isDarkMode ? Colors.tyrianPurple : Colors.peach,
+                    fontSize: 16,
+                    fontWeight: '600',
+                  }}>
+                  Button
+                </Text>
+              </Pressable>
+            </View>
           </View>
-        </View>
-        <View style={styles.thirdSection}>
-          <Pressable
-            style={{
-              backgroundColor: buttonColor,
-              marginHorizontal: 50,
-              borderRadius: 10,
-              padding: 10,
-              alignItems: 'center',
-              elevation: 3,
-            }}
-            onPressIn={() => {
-              setButtonColor(Colors.rose);
-              console.log('in');
-            }}
-            onPressOut={() => {
-              setButtonColor(Colors.tyrianPurple);
-              console.log('out');
-            }}
-            onPress={() => {
-              console.log('first');
-            }}>
-            <Text style={{color: 'white', fontSize: 16, fontWeight: '600'}}>
-              Button
-            </Text>
-          </Pressable>
-        </View>
-      </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
--- src/components/TextInput/FirstTextInput.js
+++ src/components/TextInput/FirstTextInput.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, useColorScheme} from 'react-native';
 import {Colors} from '../../interface/colors/mainColors';
 
 export const FirstTextInput = ({valueName, placeHolderName}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   const [textInputBorderWith, setTextInputBorderWith] = useState(0);
   return (
     <View style={styles.container}>
       <View style={styles.textInputNameContainer}>
-        <Text style={styles.textInputName}>{placeHolderName}</Text>
+        <Text style={styles.textInputName(isDarkMode)}>{placeHolderName}</Text>
       </View>
       <TextInput
         placeholder={valueName}
-        style={styles.textInputBox(textInputBorderWith)}
+        style={styles.textInputBox(textInputBorderWith, isDarkMode)}
         onFocus={() => {
           setTextInputBorderWith(2);
         }}
@@ -27,14 +29,15 @@ const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: 'center'},
   textInputNameContainer: {
     marginLeft: 10,
+    marginBottom: 4,
   },
-  textInputName: {
-    color: Colors.plum,
+  textInputName: isDarkMode => ({
+    color: isDarkMode ? Colors.peach : Colors.tyrianPurple,
     fontSize: 13,
     fontWeight: '600',
-  },
-  textInputBox: textInputBorderWith => ({
-    backgroundColor: '#F8E9C9',
+  }),
+  textInputBox: (textInputBorderWith, isDarkMode) => ({
+    backgroundColor: isDarkMode ? '#58002C' : '#F8E9C9',
     padding: 10,
     borderBottomWidth: textInputBorderWith,
     borderRightWidth: textInputBorderWith,
